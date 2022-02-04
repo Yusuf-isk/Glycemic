991abb43663bdbb628effa0ba136a4e50fbe8301
Yusuf IŞIK <yyusufisik1@gmail.com>
2022-02-04 19:07:35 +0300

    Initial commit

---
+++ src/main/java/com/works/glycemic/GlycemicApplication.java
@@ -0,0 +1,13 @@
+package com.works.glycemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlycemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlycemicApplication.class, args);
+    }
+
+}
